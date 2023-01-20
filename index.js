ac2a9541eefe1a5f5a7ae94fde628ca0eb9b905e
harshit-verdis <i-harshit@verdis.ai>
2023-01-20 12:04:56 +0530

    mong db added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("harshit")
+const mongodb = require('mongodb');
+const { mongo } = require('mongoose');
+const MongoClient = mongodb.MongoClient;
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'Temp1'
+
+MongoClient.connect(connectionUrl,{useNewUrlParser: true},(error,client)=>{
+    if(error)
+        console.log("UNable to connect to database")
+    else
+        console.log("Connected to database")
+
+})
